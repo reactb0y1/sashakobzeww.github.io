447633bb3d4e655204a91bb2659c88cfef814e63
Alexandr <sashakobzeww@gmail.com>
2019-05-27 16:04:34 +0300

    chr Изменили кавычки с двойных на одинарные

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 // Step 1: Scan for a device with 0xffe5 service
 navigator.bluetooth.requestDevice({
-    filters: [{ services: ["cool1"] }]
+    filters: [{ services: ['cool1'] }]
 })
     .then(function(device) {
         // Step 2: Connect to it
