31409c710614e95405e9b07f9546582b1082a8a2
Alexandr <sashakobzeww@gmail.com>
2019-05-27 12:07:10 +0300

    chr Пробуем обойтись и без подключения

--- common.js
+++ common.js
@@ -1,10 +1,10 @@
 // Step 1: Scan for a device with 0xffe5 service
 navigator.bluetooth.requestDevice({
     filters: [{ services: ["cool1"] }]
-})
+})/*
     .then(function(device) {
         // Step 2: Connect to it
         return device.gatt.connect();
-    })
+    })*/
 
 console.log("---", );
